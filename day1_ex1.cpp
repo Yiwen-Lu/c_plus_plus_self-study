2d88ae076ae50df4522cbcdf29edfcfeabfe7f3e
Yiwen Lu <35602732+Yiwen-Lu@users.noreply.github.com>
2018-08-11 23:58:33 +0200

    Add files via upload

---
+++ day1_ex1.cpp
@@ -0,0 +1,10 @@
+# include <iostream>
+int main()
+{
+	int x = 5;
+	int y = 7;
+	std::cout << "\n" << std::endl;
+	std::cout << x + y << " " << x * y << std::endl;
+	std::cout << "\n" << std::endl;
+	return 0;
+}
