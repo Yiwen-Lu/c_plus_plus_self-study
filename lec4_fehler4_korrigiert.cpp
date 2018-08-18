9d706d3d6bf7cbb733edeabd65de0851e21fcc37
Yiwen <35602732+Yiwen-Lu@users.noreply.github.com>
2018-08-18 22:07:48 +0200

    Add files via upload

---
+++ lec4_fehler4_korrigiert.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::cout << "Gebe deinen Namen ein:" << std::endl;
+
+    std::string eingabe;
+    std::cin >> eingabe;
+
+    std::cout << "Hallo " << eingabe << std::endl;
+
+    return 0;
+}
