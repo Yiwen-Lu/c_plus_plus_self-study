d0de2478e8de928311d9582d1db1d170616af2e8
Yiwen <35602732+Yiwen-Lu@users.noreply.github.com>
2018-08-21 00:09:19 +0200

    Add files via upload

---
+++ lec9_if2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+	int a, b;
+
+	std::cout << "Gebe eine Zahl ein: ";
+	std::cin >> a;
+
+	std::cout << "Gebe noch eine Zahle ein: ";
+	std::cin >> b;
+
+	if (b == 0) {
+		std::cerr << "Die zweite Zahl darf nicht 0 sein!" << std::endl;
+	} else {
+		std::cout << "Ihr Rest ist " << a % b << std::endl;
+	}
+	
+	return 0;
+}
