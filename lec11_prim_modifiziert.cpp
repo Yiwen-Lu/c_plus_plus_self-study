4527bdda8c3274a060c488ef318b720aba5a9068
Yiwen <35602732+Yiwen-Lu@users.noreply.github.com>
2018-08-21 14:28:44 +0200

    Add files via upload

---
+++ lec11_prim_modifiziert.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main() {
+	int n;
+	std::cout << "Gebe eine (postive) Zahl ein: " << std::endl;
+	std::cin >> n;
+
+	if (n == 0) {
+		std::cerr << "Die Zahl soll postiv sein!" << std::endl;
+		return 1;
+	} else if (n == 1) {
+		std::cout << n << " ist keine Primzahl." << std::endl;
+		return 0;
+	} else {
+		int i = 2;
+		while (i < n) {
+			if (n % i == 0) {
+				std::cout << n << " ist keine Primzahl." << std::endl;
+				return 0;
+			}
+			i = i + 1;
+		}
+		std::cout << n << " ist eine Primzahl." << std::endl;
+		return 0;
+	}
+}
