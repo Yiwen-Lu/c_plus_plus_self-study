fc84860906c0c325db155166a9620ca57f6c19b2
Yiwen <35602732+Yiwen-Lu@users.noreply.github.com>
2018-08-23 00:48:14 +0200

    Add files via upload

---
+++ lec14_guess.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main() {
+	int true_num, guess_num;
+	srand (time(NULL)); // set random seed
+	true_num = rand() % 6 + 1; // set the range
+
+	do {
+		std::cout << "Please guess the dice number (1 to 6): " << std::endl;
+		std::cin >> guess_num;
+		if (guess_num > true_num) {
+			std::cout << "The number is lower." << std::endl;
+		} else if (guess_num > true_num) {
+			std::cout << "The number is higher." << std::endl;
+		}
+	} while (guess_num != true_num);
+
+	std::cout << "Bingo! The dice number is " << true_num << std::endl;
+
+	return 0;
+}
