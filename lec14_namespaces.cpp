e915b74d60d2f8fc568e83b0e12812c3d87eb1d0
Yiwen <35602732+Yiwen-Lu@users.noreply.github.com>
2018-08-22 23:47:49 +0200

    Add files via upload

---
+++ lec14_namespaces.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+namespace eins {
+	int x = 5;
+}
+
+namespace zwei {
+	double x = 2.718281828459;
+}
+
+int main() {
+	std::cout << eins::x << std::endl;
+	std::cout << zwei::x << std::endl;
+	return 0;
+}
