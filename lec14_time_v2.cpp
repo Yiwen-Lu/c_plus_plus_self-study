b2b61a4b53036a0a8c924fccd2c580c58c57d945
Yiwen <35602732+Yiwen-Lu@users.noreply.github.com>
2018-08-23 00:15:44 +0200

    Add files via upload

---
+++ lec14_time_v2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <ctime>
+
+int main() {
+	// Ref: http://www.cplusplus.com/reference/ctime/localtime/
+
+	time_t rawtime;
+	struct tm * timeinfo;
+
+	time (&rawtime);
+	timeinfo = localtime (&rawtime);
+	printf ("Current local time and date: %s \n", asctime(timeinfo));
+
+	return 0;
+}
