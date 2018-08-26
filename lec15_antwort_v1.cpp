8ab9e5ec9879eaf0090a20394dd21354ca6bfb4c
Yiwen <35602732+Yiwen-Lu@users.noreply.github.com>
2018-08-26 17:41:45 +0200

    Add files via upload

--- lec15_antwort_v1.cpp
+++ lec15_antwort_v1.cpp
@@ -8,22 +8,21 @@ int main() {
 	std::vector<std::string> Antwortmoeglichkeit;
 	srand(time(NULL));
 
-	int i = 0;
-	std::string Frage;
-	std::string Antwort;
-
 	std::cout << "Gebe eine Frage ein: " << std::endl;
-	std::cin >> Frage;
+	std::string Frage;
+	getline (std::cin, Frage);
 
+	int i = 0;
 	while (i < 10) {
 		std::cout << "Gebe eine Antwort ein: " << std::endl;
-		std::cin >> Antwort;
+		std::string Antwort;
+		getline (std::cin, Antwort);
 		Antwortmoeglichkeit.push_back(Antwort);
 		i = i + 1;
 	}
 
 	int RandIndex = rand() % 10;
 	std::cout << "Zu deiner Frage \"" << Frage << 
-		"\" wurde folgende Antwort ausgesucht:" << 
+		"\" wurde folgende Antwort ausgesucht: " << 
 		Antwortmoeglichkeit[RandIndex] << std::endl;
 }
