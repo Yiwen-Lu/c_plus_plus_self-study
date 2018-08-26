4f24c2c7454e04f53bcf3f86f8e5ccc02e4dedf8
Yiwen <35602732+Yiwen-Lu@users.noreply.github.com>
2018-08-26 12:49:21 +0200

    Add files via upload

--- lec15_vector.cpp
+++ lec15_vector.cpp
@@ -5,21 +5,19 @@
 
 int main() {
 	std::vector<int> einvector;
-
-	strand(time(Null));
+	srand(time(NULL));
 
 	int i = 0;
 	while (i < 100) {
-		einvector.push_back(rand());
+		einvector.push_back(rand()); // Add element at the end
 		i = i + 1;
 	}
-
 
 	std::cout << "An Index  0 steht " << einvector[0] << std::endl;
 	std::cout << "An Index 42 steht " << einvector[42] << std::endl;
 	std::cout << "An Index 99 steht " << einvector[99] << std::endl;
 
-	// und nun alle werte auf 42 setzen ;-)
+	// und nun alle werte auf 42 setzen:
 	i = 0;
 	while (i < einvector.size()) {
 		einvector[i] = 42;
