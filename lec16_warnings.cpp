25367fd7d658fed3ffccc3bb0f5398d94848bc91
Yiwen <35602732+Yiwen-Lu@users.noreply.github.com>
2018-08-29 12:26:45 +0200

    Add files via upload

---
+++ lec16_warnings.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+	int n;
+	std::cout << "Gebe eine Zahl ein: " << std::endl;
+	std::cin >> n;
+
+	if (n == 0) {
+		std::cout << "n = " << n
+			<< ", also ist n Null!" << std::endl;
+	} else {
+		std::cout << "n = " << n
+			<< ", also ist n nicht Null!" << std::endl;
+	}
+	return 0;
+}
