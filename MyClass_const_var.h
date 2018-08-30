a7d6a71b65379a2c3ccecee15589f36849295cef
Yiwen <35602732+Yiwen-Lu@users.noreply.github.com>
2018-08-30 18:11:05 +0200

    Add files via upload

---
+++ MyClass_const_var.h
@@ -0,0 +1,12 @@
+#ifndef MYCLASS_CONST_VAR_H
+#define MYCLASS_CONST_VAR_H
+
+class MyClass {
+public:
+	MyClass(int a, int b);
+private:
+	int regVar;
+	const int constVar;
+};
+
+#endif // MYCLASS_CONST_H
