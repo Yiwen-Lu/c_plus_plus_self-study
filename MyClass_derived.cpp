80a9010d0f61bfa80dcbd35b037cac0ccb444628
Yiwen <35602732+Yiwen-Lu@users.noreply.github.com>
2018-08-30 21:22:30 +0200

    Add files via upload

---
+++ MyClass_derived.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Mother {
+public:
+	Mother() {};
+	void sayHi() {
+		cout << "Hi" << endl;
+	}
+};
+
+class Daughter: public Mother
+{
+public:
+	Daughter() {};
+};
+
+int main() {
+	Daughter d;
+	d.sayHi();
+}
