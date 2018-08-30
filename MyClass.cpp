1430c9da1f51a63c3e26d084a48d8078f211d7ca
Yiwen <35602732+Yiwen-Lu@users.noreply.github.com>
2018-08-30 18:09:42 +0200

    Add files via upload

---
+++ MyClass.cpp
@@ -0,0 +1,16 @@
+#include "MyClass.h"
+#include <iostream>
+using namespace std;
+
+MyClass::MyClass() {
+
+}
+
+void MyClass::myPrint() {
+	cout << "Hello" << endl;
+}
+
+int main() {
+	MyClass obj;
+	obj.myPrint();
+}
