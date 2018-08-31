53e2d4a1c3c454d6b46e0c926b88576ab99de67f
Yiwen <35602732+Yiwen-Lu@users.noreply.github.com>
2018-08-31 20:19:58 +0200

    Add files via upload

---
+++ func_exception_num.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	try {
+		int num1;
+		cout << "Enter the first number: " << endl;
+		cin >> num1;
+
+		int num2;
+		cout << "Enter the second number: " << endl;
+		cin >> num2;
+
+		if (num2 == 0) {
+			throw 0;
+		}
+
+		cout << "Result: " << num1 / num2 << endl;
+
+	}
+
+	catch(int x) {
+		cout << "Division by zero!" << endl;
+	}
+}
